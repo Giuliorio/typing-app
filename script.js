4f15049998268efa16b6000c89032c5a02b6e0b2
Giulio Riondino <riondino.giulio@gmail.com>
2024-08-13 22:24:25 -0700

    Create typing logic

--- script.js
+++ script.js
@@ -15,4 +15,21 @@ function makeSentence() {
     });
 }
 
+function getFirstInactive() {
+    return [...text.children].filter(span => span.classList.length < 1)[0]
+}
+
+function getLastActive() {
+    const typed = [...text.children].filter(span => span.classList.length > 0)
+    return typed[typed.length - 1]
+}
+
 makeSentence()
+
+window.addEventListener('keydown', (e) => {
+    const first = getFirstInactive()
+
+    if (e.key === 'Backspace') return getLastActive().classList.remove('wrong', 'right')
+    if (e.key === first.textContent) first.classList.add('right')
+    if (e.key !== first.textContent && e.key !== 'Shift') first.classList.add('wrong')
+})
